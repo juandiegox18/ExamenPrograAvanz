344ebd060e1728755846cfd7778946b11f7af254
juandiegox18 <juandiego17@gmail.com>
2020-03-04 17:13:42 -0600

    Revert "Examen I"
    This reverts commit cf6b449109bb2d678f50b8ef9ac375175749199b.

--- Examen-Uam/services/read_service.py
+++ Examen-Uam/services/read_service.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 filename = "PADRON_COMPLETO.txt"
 # tupla con los valores por defecto de cada votante
 values = ("Cedula", "Codelec", "Sexo", "FechaCaduc",
@@ -20,8 +18,6 @@ def leer_votantes():
             for i in range(len(s)):
                 # se agrega el key value a cada campo del votante
                 votante[values[i]] = s[i]
-            votante["FechaCaduc"] = datetime.strptime(
-                votante["FechaCaduc"], '%Y-%m-%d').date()
             votantes.append(votante)
         return votantes
 
@@ -42,8 +38,6 @@ def obtener_mujeres_votantes():
                 votante[values[i]] = s[i]
             if(votante["Sexo"] == '2'):
                 # si el votante es de sexo femenino se agrega al array
-                votante["FechaCaduc"] = datetime.strptime(
-                    votante["FechaCaduc"], "%Y-%m-%d").date()
                 votantesMujeres.append(votante)
         return votantesMujeres
 
@@ -65,7 +59,5 @@ def obtener_votantes_distrito(id):
                 votante[values[i]] = s[i]
             if(votante["Codelec"] == id):
                 # si el votante es de distrito recibido por el id se agrega al array
-                votante["FechaCaduc"] = datetime.strptime(
-                    votante["FechaCaduc"], "%Y-%m-%d").date()
                 votantesPorDistrito.append(votante)
         return votantesPorDistrito
